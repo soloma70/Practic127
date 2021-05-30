ec8bf97ab7aee7f6ced4279ca7598de8d98860f6
soloma70 <soloma70@gmail.com>
2021-05-30 20:13:28 +0300

    Corr names acc. to cond.

--- pr1273.py
+++ pr1273.py
@@ -1,8 +1,8 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}  # Объявление словаря процентов
-mon = float(input("Введите сумму вклада: "))  # Ввод суммы
-dep = [int(round(mon * per_cent['ТКБ'] / 100, 0)), int(round(mon * per_cent['СКБ'] / 100, 0)),
-       int(round(mon * per_cent['ВТБ'] / 100, 0)), int(round(mon * per_cent['СБЕР'] / 100, 0))]  # Вычисление сумм депозитов
+money = float(input("money = "))  # Ввод суммы
+deposit = [int(round(money * per_cent['ТКБ'] / 100, 0)), int(round(money * per_cent['СКБ'] / 100, 0)),
+       int(round(money * per_cent['ВТБ'] / 100, 0)), int(round(money * per_cent['СБЕР'] / 100, 0))]  # Вычисление сумм депозитов
 keys = list(per_cent.keys())  # Преобразование ключей в список
 print("          ", keys[0], " ", keys[1], " ", keys[2], " ", keys[3])  # Вывод заголовков
-print("deposit =", dep)  # Вывод сумм депозитов
-print("Максимальная сумма, которую вы можете заработать —", max(dep))  # Вывод макс.суммы
+print("deposit =", deposit)  # Вывод сумм депозитов
+print("Максимальная сумма, которую вы можете заработать —", max(deposit))  # Вывод макс.суммы
